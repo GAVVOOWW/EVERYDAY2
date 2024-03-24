450fc61476afa2b2cc80d4cd52140eb8d17ccc3b
GAVVOOWW <156282937+GAVVOOWW@users.noreply.github.com>
2024-03-24 17:01:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+$(function(){
+    $('#toggleThis1').hover(function(){
+        $('#content1').slideToggle();
+        
+    });
+
+});
+
+$(function(){
+    $('#toggleThis2').hover(function(){
+        $('#content2').slideToggle();
+    });
+
+});
+
+$(function(){
+    $('#toggleThis3').hover(function(){
+        $('#content3').slideToggle();
+    });
+
+});
+
+$(function(){
+    $('#toggleThis4').hover(function(){
+        $('#content4').slideToggle();
+    });
+
+});
+
+$(function(){
+    $('#toggleThis5').hover(function(){
+        $('#content5').slideToggle();
+    });
+
+});
+
+$(function(){
+    $('#toggleThis6').hover(function(){
+        $('#content6').slideToggle();
+    });
+
+});
+
+$(function(){
+    $('#toggleThis7').hover(function(){
+        $('#content7').slideToggle();
+    });
+
+});
